c076f47c947000de7dd684a66d8b4b2c882531a5
Tsukinai <909436488@qq.com>
2024-10-07 03:59:06 +0800

    feat: 添加登录功能

--- src/main/java/com/tsukinai/controller/UserController.java
+++ src/main/java/com/tsukinai/controller/UserController.java
@@ -3,6 +3,7 @@ package com.tsukinai.controller;
 import com.tsukinai.pojo.Result;
 import com.tsukinai.pojo.User;
 import com.tsukinai.service.UserService;
+import com.tsukinai.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,24 @@ public class UserController {
             return Result.success();
         }
 
+    }
+
+    //登录
+    @RequestMapping("/login")
+    public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
+        //查询数据库是否存在该用户
+        User loginUser = userService.findByUsername(username);
+        if (loginUser == null) {
+            //用户不存在
+            return Result.error("用户不存在");
+        } else {
+            //判断密码是否正确
+            if (loginUser.getPassword().equals(Md5Util.getMD5String(password))) {
+                return Result.success("登录成功");
+            } else {
+                return Result.error("密码错误");
+            }
+        }
     }
 
 }
