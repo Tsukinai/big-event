002fdd5d772ce8ae2a7c91bb8997e639d34c4cb8
Tsukinai <909436488@qq.com>
2024-10-07 03:46:52 +0800

    feat: 使用spring validation对参数进行校验

--- src/main/java/com/tsukinai/controller/UserController.java
+++ src/main/java/com/tsukinai/controller/UserController.java
@@ -3,6 +3,7 @@ package com.tsukinai.controller;
 import com.tsukinai.pojo.Result;
 import com.tsukinai.pojo.User;
 import com.tsukinai.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,17 +17,18 @@ public class UserController {
 
     //注册
     @RequestMapping("/register")
-    public Result register(String username, String password) {
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
         //查询数据库是否存在该用户
         User u = userService.findByUsername(username);
-        if(u != null) {
+        if (u != null) {
             //用户已存在
             return Result.error("该用户已存在");
-        }else {
+        } else {
             //注册
             userService.register(username, password);
             return Result.success();
         }
+
     }
 
 }
