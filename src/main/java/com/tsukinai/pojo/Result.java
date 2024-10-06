fc9db6d934011feb6c3fac1f7e337fb4f6e68a08
Tsukinai <909436488@qq.com>
2024-10-07 03:28:40 +0800

    fix(pojo): 给result类增加Data注解

--- src/main/java/com/tsukinai/pojo/Result.java
+++ src/main/java/com/tsukinai/pojo/Result.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 //统一响应结果
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class Result<T> {
     private Integer code;//业务状态码  0-成功  1-失败
     private String message;//提示信息
