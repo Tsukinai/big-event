444e8c21ab2bab4acde82721c80c8b401c58a31f
Tsukinai <909436488@qq.com>
2024-10-07 02:46:26 +0800

    feat: 初始化项目

---
+++ src/main/java/com/tsukinai/bigEventApplication.java
@@ -0,0 +1,13 @@
+package com.tsukinai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class bigEventApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(bigEventApplication.class, args);
+    }
+}
