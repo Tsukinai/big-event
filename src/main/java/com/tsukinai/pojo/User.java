f05b1115088af3a665dacb4710897a9aa5b2e790
Tsukinai <909436488@qq.com>
2024-10-07 18:36:27 +0800

    feat: 增加JsonIgnore注解，不返回密码给前端

--- src/main/java/com/tsukinai/pojo/User.java
+++ src/main/java/com/tsukinai/pojo/User.java
@@ -2,6 +2,7 @@ package com.tsukinai.pojo;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,7 +11,10 @@ import java.time.LocalDateTime;
 public class User {
     private Integer id;//主键ID
     private String username;//用户名
+
+    @JsonIgnore//密码不返回给前端
     private String password;//密码
+
     private String nickname;//昵称
     private String email;//邮箱
     private String userPic;//用户头像地址
